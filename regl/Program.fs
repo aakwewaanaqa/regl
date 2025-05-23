7fc261539d975c5a808ff50c2bd6e28969d76f75
ponito <vvbdac58@gmail.com>
2025-05-23 23:53:24 +0800

    ...

--- regl/Program.fs
+++ regl/Program.fs
@@ -1,2 +1,30 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+module Program
+
+open System
+open Argu
+
+type Arguments =
+    | [<AltCommandLine("-e")>] Pattern of string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Pattern _ -> "To gives pattern of Regex..."
+
+[<EntryPoint>]
+let main args =
+
+    let pipe = Console.In.ReadToEnd()
+    printfn $"{pipe}"
+    
+    let parser = ArgumentParser.Create<Arguments>()
+    try
+        let results = parser.ParseCommandLine args
+        ()
+    with
+    | :? ArguParseException as ex ->
+        // 印出 Argu 自動產生的使用說明
+        printfn "%s" ex.Message
+
+    0
