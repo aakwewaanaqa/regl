a5c880a15c50a5ec5356b659291273118bd7f4bc
ponito <vvbdac58@gmail.com>
2025-05-27 22:38:04 +0800

    Keeps testing...

--- xtests/ReflectionTests.fs
+++ xtests/ReflectionTests.fs
@@ -1,28 +1,47 @@
 namespace XTests
 
 open System
+open System.Reflection
 open Regl.Program
 open Xunit
+open Xunit.Abstractions
 
-module ReflectionTests =
+module ReflectionTesting =
 
-    type Arguments() =
-        member val cmd: Commands = Copy with get, set
-        member val f: bool = false with get, set
+    type CommandCandidatesAttribute([<ParamArray>]types: Type array) =
+        inherit Attribute()
+        member this.candidates = types
 
-    and Commands =
-        | Copy
-        | Split
+    type Entry(
+        [<CommandCandidates(
+            typeof<Copy>,
+            typeof<Split>)>]
+        cmd: ICommand) =
+        member this.cmd = cmd
 
-    let argType = typeof<Arguments>
+    and ICommand =
+        interface end
 
-    let True b = Assert.True b
+    and Copy() =
+        interface ICommand
 
-    let hasField field =
-        let fieldInfo = argType.GetField field
-        fieldInfo <> null
+    and Split(delimiter: string) =
+        interface ICommand
+        member this.delimiter = delimiter
 
-    [<Fact>]
-    let ``Tests Union Type`` () =
-        True(hasField "cmd")
-        ()
+    let hasField (t: Type) field =
+        t.GetRuntimeFields() |> Seq.tryFind (fun f -> f.Name = field) |> Option.isSome
+
+    type ReflectionTests(output: ITestOutputHelper) =
+
+        let log a = output.WriteLine $"{a}"
+
+        [<Fact>]
+        let ``Tests Concrete Type`` () =
+            let t = typeof<Entry>
+
+            t.GetConstructors()
+            |> Array.iter (fun ctor ->
+                           log ctor
+                           ctor.GetParameters() |> Array.iter log)
+            ()
