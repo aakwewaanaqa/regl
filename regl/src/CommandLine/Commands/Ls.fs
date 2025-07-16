8860d5040b8834fe44e17698ef89505951db7a3a
ponito <vvbdac58@gmail.com>
2025-07-16 15:32:27 +0800

    **Add `--ignore-file` flag to `ls` command for exclusion patterns**
    **繁體中文**
    新增 `--ignore-file` 旗標於 `ls` 指令，支援使用排除模式的檔案忽略功能

--- regl/src/CommandLine/Commands/Ls.fs
+++ regl/src/CommandLine/Commands/Ls.fs
@@ -1,6 +1,8 @@
 module Regl.CommandLine.Commands.Ls
 
 open System.IO
+open System.Text.RegularExpressions
+open Microsoft.Extensions.FileSystemGlobbing
 open Regl
 open Regl.CommandLine.IO.InOut
 open Regl.CommandLine.Types.FlagsAndParams
@@ -18,6 +20,7 @@ let entry =
     let dFlag = BoolFlag ("-d", "directories")
     let fFlag = BoolFlag ("-f", "files")
     let RFlag = BoolFlag ("-R", "recursively")
+    let ignoreFileFlag = StringFlag("--ignore-file", "for patterns of git ignore")
 
     let patternFlag =
         StringFlag ("--pattern", "files or directories matching .net pattern")
@@ -27,10 +30,11 @@ let entry =
         fFlag :> IFlag
         RFlag :> IFlag
         patternFlag :> IFlag
+        ignoreFileFlag :> IFlag
     ]
     
     let exeLs : ArgBehaviour =
-        fun dto ->
+        fun dto ->            
             let pattern = dto.flags.firstOrDefault patternFlag ""
             let pwd = Directory.GetCurrentDirectory ()
             let isFile = dto.flags.containsFlag fFlag
@@ -40,16 +44,35 @@ let entry =
             let option =
                 if isRecursively then SearchOption.AllDirectories
                 else SearchOption.TopDirectoryOnly
+                
+            let ignoreFile = dto.flags.firstOrDefault ignoreFileFlag ""
             
-            let files = Directory.GetFiles (pwd, pattern, option)
-            let dirs = Directory.GetDirectories (pwd, pattern, option)
-
-            let paths =
-                if isFile && isDir then files |> Array.append dirs
-                elif isDir then dirs
-                else files
-
-            paths |> List.ofArray |> (fun lines -> Out.lines <- lines)
+            let paths =           
+                let founds =                                            
+                    if isFile && isDir then
+                        Directory.GetFiles (pwd, pattern, option)
+                        |> Array.append (Directory.GetDirectories (pwd, pattern, option))
+                    elif isDir then
+                        Directory.GetDirectories (pwd, pattern, option)
+                    else
+                        Directory.GetFiles (pwd, pattern, option)            
+                
+                if ignoreFile |> System.String.IsNullOrWhiteSpace |> not then
+                    let matcher = Matcher()
+                    
+                    File.ReadAllLines ignoreFile
+                    |> Array.map(_.Trim())
+                    |> Array.filter(fun pattern -> pattern.StartsWith('#') |> not)
+                    |> fun patterns -> matcher.AddExcludePatterns patterns
+       
+                    matcher.Match(founds).Files
+                    |> Seq.map(_.Path)
+                    |> List.ofSeq
+                else
+                    founds
+                    |> List.ofArray
+                    
+            paths |> (fun lines -> Out.lines <- lines)
 
     CmdEntry(cmdName).addInfo(cmdInfo)
     |> CmdEntry.acceptCombos combos exeLs
--- xtests/Commands/Ls/Tests.fs
+++ xtests/Commands/Ls/Tests.fs
@@ -1,7 +1,10 @@
 module Commands.Ls.Tests
 
+open System.IO
+open System.Text.RegularExpressions
 open Regl.CommandLine.Commands
 open Regl.CommandLine.Commands.Shared
+open Regl.CommandLine.IO
 open Regl.CommandLine.Types.Arguments
 open XTests.Types
 open Xunit
@@ -27,7 +30,56 @@ type Tests (helper : ITestOutputHelper) =
     [<InlineData("ls -Rf --pattern '*'")>]
     [<InlineData("ls -Rdf --pattern '*'")>]
     let ``test ls`` (args : string) =
-        (16, Ls.entry.entries.Length) |> Assert.Equal
+        (32, Ls.entry.entries.Length) |> Assert.Equal
         ("ls", Ls.entry.name) |> Assert.Equal
 
         Args args |> executeEntries [| Ls.entry |] |> ignore
+
+    [<Fact>]
+    let ``test fact`` () =
+        
+        [|
+            "/home/c0054/UnityProjects/regl/xtests/bin/Release/net9.0/mine.env"
+            "/home/c0054/UnityProjects/regl/xtests/bin/Release/net9.0/main.pyc"
+        |]
+        |> Array.filter(fun path ->
+            [|
+                Regex(".*\.env")
+                Regex(".*\.pyc")
+            |]
+            |> Array.exists(_.IsMatch(path))
+            |> not
+        )
+        |> fun array -> Assert.True(array.Length = 0)
+    
+    [<Theory>]
+    [<InlineData("ls --ignore-file='.gitignore'")>]
+    let ``test ls -f --ignore-file`` (args : string) =
+        // Arrange
+        // Prepare gitignore and files
+        let gitignore = "
+*.env
+*.pyc
+*.doc*
+        "
+        File.WriteAllText(".gitignore", gitignore)
+        
+        let someTxt = "123"
+        File.WriteAllText("something.txt", someTxt)
+        File.WriteAllText("nothing.txt", someTxt)
+        File.WriteAllText("anything.txt", someTxt)
+        File.WriteAllText("mine.env", someTxt)
+        File.WriteAllText("main.pyc", someTxt)
+
+        Directory.CreateDirectory(".doc") |> ignore        
+        File.WriteAllText(".doc/main.pyc", someTxt)
+        
+        // Act
+        let entry = Args args
+                    |> executeEntries [| Ls.entry |]
+             
+        // Assert
+        helper.WriteLine(InOut.Out.all)
+        Assert.False(InOut.Out.all.Contains(".pyc"))
+        Assert.False(InOut.Out.all.Contains(".env"))
+        Assert.False(InOut.Out.all.Contains(".doc"))
